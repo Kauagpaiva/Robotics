bac7dff65557c4f4e64c2257433b0a8b6439774c
Kauagpaiva <skaliburgpc@gmail.com>
2024-05-26 10:46:15 -0300

    OOP

--- build/my_py_pkg/build/lib/my_py_pkg/my_first_node.py
+++ build/my_py_pkg/build/lib/my_py_pkg/my_first_node.py
@@ -3,15 +3,21 @@
 import rclpy
 from rclpy.node import Node
 
+class My_node(Node):
+    def __init__(self):
+        super().__init__('py_test')
+        self.counter_ = 0
+        self.get_logger().info("Hello world ros2")
+        self.create_timer(0.5, self.timer_callback) # Every 0.5 seconds it calls the timer_callback function
+
+    def timer_callback(self):
+        self.counter_ += 1
+        self.get_logger().info("hello " + str(self.counter_))
+
 def main(args=None):
     rclpy.init(args=args) # initializing ros2 communication
-
-    node = Node('py_test') # creating a node object (the arg is the name of the node)
-
-    node.get_logger().info("Hello world ros2") # print hello world
-
+    node = My_node()
     rclpy.spin(node)
-
     rclpy.shutdown() # ends ros2 communication
 
 if __name__ == "__main__":
